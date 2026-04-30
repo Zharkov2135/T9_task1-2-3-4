231ce1bbf22c6ecf262167a9f3b2e44f974c4325
Жарков Олександр <o.zharkov@istu.edu.ua>
2026-04-30 23:18:10 +0300

    Add files via upload

---
+++ T9_task2/T9_task2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double R, r;
+    cout << "Vvedit zovnishnii radius R: ";
+    cin >> R;
+    cout << "Vvedit vnutrishnii radius r: ";
+    cin >> r;
+
+    if (R <= 0 || r < 0) {
+        cout << "Pomylka: R > 0, r >= 0.\n";
+        return 0;
+    }
+    if (r > R) {
+        cout << "Pomylka: r ne mozhe buty bilshym za R.\n";
+        return 0;
+    }
+
+    const double PI = acos(-1.0);
+    double S = PI * (R * R - r * r);
+
+    cout << fixed << setprecision(6);
+    cout << "Ploshcha kiltsia S = " << S << "\n";
+    return 0;
+}
