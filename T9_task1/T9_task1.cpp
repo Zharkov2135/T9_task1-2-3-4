4df5875b32fdaa716e41757ecefb12eb8dccacdb
Жарков Олександр <o.zharkov@istu.edu.ua>
2026-04-30 23:19:03 +0300

    Add files via upload

---
+++ T9_task1/T9_task1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double S;
+    cout << "Vvedit ploshchu kvadrata S: ";
+    cin >> S;
+
+    if (S <= 0) {
+        cout << "Pomylka: ploshcha maie buty dodatnoiu.\n";
+        return 0;
+    }
+
+    double a = sqrt(S);
+    double P = 4 * a;
+
+    cout << fixed << setprecision(6);
+    cout << "Storona a = " << a << "\n";
+    cout << "Perimetr P = " << P << "\n";
+    return 0;
+}
