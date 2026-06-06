b8052b29ea548e9fb4c20d21ffc5f42eaf913e02
Жарков Олександр <o.zharkov@istu.edu.ua>
2026-06-06 22:37:06 +0300

    Add files via upload

--- T9_task4/T9_task4.cpp
+++ T9_task4/T9_task4.cpp
@@ -1,1 +1,23 @@
+﻿#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cout << "Vvedit dvotsyfrovoe chyslo: ";
+    cin >> n;
+
+    int absn = (n < 0) ? -n : n;
+    if (absn < 10 || absn > 99) {
+        cout << "Pomylka: potribno vvesty dvotsyfrovoe chyslo (10..99 abo -10..-99).\n";
+        return 0;
+    }
+
+    int tens = absn / 10;
+    int ones = absn % 10;
+
+    cout << "Kilkist desiatkiv = " << tens << "\n";
+    cout << "Kilkist odynyts = " << ones << "\n";
+    cout << "Suma tsyfr = " << (tens + ones) << "\n";
+    cout << "Dobutok tsyfr = " << (tens * ones) << "\n";
+    return 0;
+}
