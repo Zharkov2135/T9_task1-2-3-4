d12e3bc2399712eafd932ceefbca5427aee8dd59
Жарков Олександр <o.zharkov@istu.edu.ua>
2026-06-06 22:38:00 +0300

    Add files via upload

--- T9_task3/T9_task3.cpp
+++ T9_task3/T9_task3.cpp
@@ -1,1 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
 
+int main() {
+    double a, b;
+    cout << "Vvedit katet a: ";
+    cin >> a;
+    cout << "Vvedit katet b: ";
+    cin >> b;
+
+    if (a <= 0 || b <= 0) {
+        cout << "Pomylka: katety maiut buty dodatni. Trykutnyk ne isnue.\n";
+        return 0;
+    }
+
+    // Priamokutnyi trykutnyk z dodatnymy katetamy isnue
+    double c = sqrt(a * a + b * b);
+    double P = a + b + c;
+
+    cout << fixed << setprecision(6);
+    cout << "Hipotenusa c = " << c << "\n";
+    cout << "Perimetr P = " << P << "\n";
+    cout << "Vysnovok: priamokutnyi trykutnyk isnue.\n";
+    return 0;
+}
